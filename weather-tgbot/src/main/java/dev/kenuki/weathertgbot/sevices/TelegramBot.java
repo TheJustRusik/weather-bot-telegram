42a3e3dc74a5c2ef2f78e346e33c0e2c8cf86a52
TheJustRusik <7thejustrusik7@gmail.com>
2024-07-08 23:17:21 +0500

    Added Dockerfile docker-compose.yml and inline keyboard test implementation

--- weather-tgbot/src/main/java/dev/kenuki/weathertgbot/sevices/TelegramBot.java
+++ weather-tgbot/src/main/java/dev/kenuki/weathertgbot/sevices/TelegramBot.java
@@ -9,7 +9,12 @@ import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
@@ -40,11 +45,45 @@ public class TelegramBot implements SpringLongPollingBot, LongPollingUpdateConsu
                     .chatId(chat_id)
                     .text("Chat id: " + chat_id + "❤️ Received message: " + msg)
                     .replyToMessageId(update.getMessage().getMessageId())
+                    .replyMarkup(InlineKeyboardMarkup
+                            .builder()
+                            .keyboardRow(
+                                    new InlineKeyboardRow(InlineKeyboardButton
+                                            .builder()
+                                            .text("🥇")
+                                            .callbackData("update_msg_text")
+                                            .build()
+                                    )
+                            ).build()
+                    )
                     .build();
             try {
                 telegramClient.execute(message);
             } catch (TelegramApiException e) {
                 log.error(e.getMessage());
+            }
+        }else if (update.hasCallbackQuery()) {
+            String call_data = update.getCallbackQuery().getData();
+            long message_id = update.getCallbackQuery().getMessage().getMessageId();
+            long chat_id = update.getCallbackQuery().getMessage().getChatId();
+
+            if(call_data.equals("update_msg_text")) {
+                SendMessage sendMessage = SendMessage.builder()
+                        .chatId(chat_id)
+                        .text("❤️")
+                        .build();
+                DeleteMessage deleteMessage = DeleteMessage
+                        .builder()
+                        .chatId(chat_id)
+                        .messageId(Math.toIntExact(message_id))
+                        .build();
+
+                try {
+                    telegramClient.execute(sendMessage);
+                    telegramClient.execute(deleteMessage);
+                } catch (TelegramApiException e) {
+                    log.error(e.getMessage());
+                }
             }
         }
     }
