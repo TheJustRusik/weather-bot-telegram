e0f7407dd95ebef092c4ee29e1012c7d195a7545
TheJustRusik <7thejustrusik7@gmail.com>
2025-03-30 18:33:43 +0500

    update

--- weather-tgbot/src/main/java/org/kenuki/weathertgbot/telegram/TelegramBot.java
+++ weather-tgbot/src/main/java/org/kenuki/weathertgbot/telegram/TelegramBot.java
@@ -7,12 +7,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.List;
 
 @Component
-public class TelegramBot implements SpringLongPollingBot, LongPollingUpdateConsumer {
+public class TelegramBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
     private final TelegramBotConfiguration telegramBotConfiguration;
     private final CommandProceedService commandProceedService;
     private final Logger log = LoggerFactory.getLogger(TelegramBot.class);
@@ -24,8 +25,8 @@ public class TelegramBot implements SpringLongPollingBot, LongPollingUpdateConsu
     }
 
     @Override
-    public void consume(List<Update> list) {
-        list.forEach(this::proceedUpdate);
+    public void consume(Update update) {
+        proceedUpdate(update);
     }
 
     private void proceedUpdate(Update update) {
